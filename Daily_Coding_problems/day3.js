7f47918d7f075adb6c08bf34c0f9909056aa69ad
itsTranT1706 <khiemtrangia60@gmail.com>
2023-10-31 10:53:52 +0700

    solve day3

---
+++ Daily_Coding_problems/day3.js
@@ -0,0 +1,34 @@
+/**
+ * Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Stripe.
+
+Given an array of integers, find the first missing positive integer in linear time and constant space. 
+In other words, find the lowest positive integer that does not exist in the array.
+ The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+ */
+function posArr(arr) {
+    return arr.filter((num)=> {
+        return num>0;
+    })
+}
+function sortArr(arr) {
+     arr.sort((a,b)=> {
+        return a-b;
+     })
+     return arr;
+}
+function mySolve(arr) {
+    let temp = posArr(arr);
+    let newArr = sortArr(temp);
+    let result=1 ;
+    while (newArr.indexOf(result)>=0) {
+        result++;
+    }
+    return result;
+
+}
+let arr = [3, 5, 1, 2];
+console.log(mySolve(arr));
