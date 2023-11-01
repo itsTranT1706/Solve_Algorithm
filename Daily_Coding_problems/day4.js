9f1a857cd360ddd3e67597975239d0782afa2eba
itsTranT1706 <khiemtrangia60@gmail.com>
2023-11-01 08:51:40 +0700

    format code

--- Daily_Coding_problems/day4.js
+++ Daily_Coding_problems/day4.js
@@ -16,24 +16,23 @@ Implement car and cdr.
  */
 
 //SOLUTION
-function cons(a,b) {
+function cons(a, b) {
     function pair(f) {
-        return f(a,b);
+        return f(a, b);
     }
     return pair;
 }
-
-function car(f) {
-    function left(a,b) {
+function car(cb) {
+    function left(a, b) {
         return a;
     }
-    return f(left);
+    return cb(left);
 }
-function cdr(f) {
-    function right(a,b) {
+function cdr(cb) {
+    function right(a, b) {
         return b;
     }
-    return f(right);
+    return cb(right);
 }
-console.log(car(cons(3,4)));
-console.log(cdr(cons(3,4)));
+console.log(car(cons(3, 4)));
+console.log(cdr(cons(3, 4)));
