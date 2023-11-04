43fa07242aa228f7e0a8423c4c2e2c047ad22095
itsTranT1706 <khiemtrangia60@gmail.com>
2023-11-04 23:31:10 +0700

    problem day5

--- Daily_Coding_problems/day4.js
+++ Daily_Coding_problems/day4.js
@@ -1,21 +1,21 @@
-/**
- * Good morning! Here's your coding interview problem for today.
+// /**
+//  * Good morning! Here's your coding interview problem for today.
 
-This problem was asked by Jane Street.
+// This problem was asked by Jane Street.
 
-cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. 
-For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+// cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. 
+// For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
 
-Given this implementation of cons:
+// Given this implementation of cons:
 
-def cons(a, b):
-    def pair(f):
-        return f(a, b)
-    return pair
-Implement car and cdr.
- */
+// def cons(a, b):
+//     def pair(f):
+//         return f(a, b)
+//     return pair
+// Implement car and cdr.
+//  */
 
-//SOLUTION
+// //SOLUTION
 function cons(a, b) {
     function pair(f) {
         return f(a, b);
@@ -36,3 +36,5 @@ function cdr(cb) {
 }
 console.log(car(cons(3, 4)));
 console.log(cdr(cons(3, 4)));
+
+
